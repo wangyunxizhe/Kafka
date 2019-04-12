a3e8b0f1ca68777649a62b4905cca215d1c6f9b3
wangyunxizhe <543456229@qq.com>
2019-04-12 13:31:53 +0800

    Kafka 发送/接收 消息简单示例

---
+++ src/main/java/com/yuan/KafkaApplication.java
@@ -0,0 +1,15 @@
+package com.yuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class KafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yuan/common/ErrorCode.java
@@ -0,0 +1,6 @@
+package com.yuan.common;
+
+public class ErrorCode {
+    public final static int SUCCESS = 200;
+    public final static int EXCEPTION = 500;
+}
